ba0f6950c0488aca95f8685839b95ab4c06df337
xFuney <funey@dsrt-int.net>
2021-05-13 10:25:46 +0100

    test ci again

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -22,6 +22,6 @@ if (process.platform == "win32") {
     app.setAppUserModelId(Constants.APP_ID)
 }
 
-Logger.log("bootstrap", "Handing off to the Window handler script...")
+Logger.log("bootstrap", "Handing off to the Window handler...")
 
 require('./main_window.js')
